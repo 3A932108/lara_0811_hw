4f9ba330f4be3aa1b3a6c4e6fbfe7c7471e22def
yi chu hsu <b28203598@yahoo.com.tw>
2022-11-04 00:05:27 +0800

    設定各路徑連結

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -45,6 +45,9 @@
                     </svg>
                 </div>
 
+                <a href="/about">about</a>
+                <a href="/home">home</a>
+
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
